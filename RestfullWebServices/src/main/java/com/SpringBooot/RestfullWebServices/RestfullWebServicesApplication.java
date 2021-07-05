2d11a04dcd7a35d52250ae05d736351b9c726f01
Saket Verma <77398288+OliverWood69@users.noreply.github.com>
2021-07-05 22:31:14 +0530

    More REST code added

--- RestfullWebServices/src/main/java/com/SpringBooot/RestfullWebServices/RestfullWebServicesApplication.java
+++ RestfullWebServices/src/main/java/com/SpringBooot/RestfullWebServices/RestfullWebServicesApplication.java
@@ -2,12 +2,33 @@ package com.springBooot.restfullWebServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class RestfullWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullWebServicesApplication.class, args);
+	}
+
+
+
+
+	// if you get error while running delete and post request(Spring basic security) TRY THIS:-
+	@Configuration
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				//.formLogin().and()
+				.httpBasic();
+		}
 	}
 
 }
--- RestfullWebServices/src/main/java/com/SpringBooot/RestfullWebServices/versioning/PersonVersionController.java
+++ RestfullWebServices/src/main/java/com/SpringBooot/RestfullWebServices/versioning/PersonVersionController.java
@@ -6,13 +6,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersionController {
 
+    // URI Versioning
     @GetMapping(path="/api/v1/person")
     public PersonV1 personV1Api(){
-        return new PersonV1("Sarad Kumar");
+        return new PersonV1("Jacob Murphy");
         }
-
     @GetMapping(path="/api/v2/person")
     public PersonV2 personV2Api(){
-        return new PersonV2(new Name("Sarad","Kumar"));
+        return new PersonV2(new Name("Jacob","Murphy"));
     }
+
+    // Request Param Versioning
+    @GetMapping(value="/person/params",params = "version=1")
+    public PersonV1 reqParamVersioning1(){
+        return new PersonV1("Jacob Murphy");
+    }
+    @GetMapping(value="/person/params",params = "version=2")
+    public PersonV2 reqParamVersioning2(){
+        return new PersonV2(new Name("Jacob","Murphy"));
+    }
+
+    // Headers Param Versioning
+    @GetMapping(value="/person/header",headers = "X-API-VERSION=1")
+    public PersonV1 headerVersioning1(){
+        return new PersonV1("Jacob Murphy");
+    }
+    @GetMapping(value="/person/header",headers = "X-API-VERSION=2")
+    public PersonV2 headerVersioning2(){
+        return new PersonV2(new Name("Jacob","Murphy"));
+    }
+
+    // Accept header or MINE type Versioning
+    @GetMapping(value="/person/produces",produces = "application/xyz.company.app-v1+json")
+    public PersonV1 producesVersioning1(){
+        return new PersonV1("Jacob Murphy");
+    }
+    @GetMapping(value="/person/produces",produces = "application/xyz.company.app-v2+json")
+    public PersonV2 producesVersioning2(){
+        return new PersonV2(new Name("Jacob","Murphy"));
+    }
+
 }
